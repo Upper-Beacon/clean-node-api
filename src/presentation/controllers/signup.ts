f7c6950ac1487ee97fea05e16a3a2725ab6d5170
Danilo Augusto <dasf_cm@terra.com.br>
2021-01-20 13:43:24 -0300

    feat: ensure SignUpController returns an error if no name is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,9 @@
 export class SignUpController {
   // eslint-disable-next-line no-unused-vars
   handle(httpRequest: any): any {
-    return { statusCode: 400 };
+    return {
+      statusCode: 400,
+      body: new Error('Missing param: name'),
+    };
   }
 }
