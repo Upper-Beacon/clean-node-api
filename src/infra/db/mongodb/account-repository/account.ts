4a0f5c3bbe2bfc81f4639c201ea7f046b2663256
Danilo Augusto <dasf_cm@terra.com.br>
2021-01-21 16:09:15 -0300

    refactor: remove unused comment

--- src/infra/db/mongodb/account-repository/account.ts
+++ src/infra/db/mongodb/account-repository/account.ts
@@ -4,7 +4,6 @@ import { AddAccountModel } from '../../../../domain/usecases/add-account';
 import { MongoHelper } from '../helpers/mongo-helper';
 
 export class AccountMongoRepository implements AddAccountRepository {
-  // eslint-disable-next-line no-unused-vars
   async add(accountData: AddAccountModel): Promise<AccountModel> {
     const accountCollection = MongoHelper.getCollection('accounts');
     const result = await accountCollection.insertOne(accountData);
