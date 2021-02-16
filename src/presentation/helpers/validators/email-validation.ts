168291bde85cbb780dea50bb15641e448d0d6a18
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-16 17:58:26 -0300

    refactor: move EmailValidation to the ValidationComposite

---
+++ src/presentation/helpers/validators/email-validation.ts
@@ -0,0 +1,17 @@
+import { InvalidParamError } from '../../errors';
+import { EmailValidator } from '../../protocols/email-validator';
+import { Validation } from './validation';
+
+export class EmailValidation implements Validation {
+  private readonly fieldName: string;
+  private readonly emailValidator: EmailValidator;
+
+  constructor(fieldName: string, emailValidator: EmailValidator) {
+    this.fieldName = fieldName;
+    this.emailValidator = emailValidator;
+  }
+
+  validate(input: any): Error {
+    return !this.emailValidator.isValid(input[this.fieldName]) ? new InvalidParamError(this.fieldName) : null;
+  }
+}
