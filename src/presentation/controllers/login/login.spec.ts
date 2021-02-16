3bc9c2447fb166ff3961b043686698500ee1946d
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-16 19:14:03 -0300

    refactor: move HttpHelper to a folder

--- src/presentation/controllers/login/login.spec.ts
+++ src/presentation/controllers/login/login.spec.ts
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable max-classes-per-file */
 import { MissingParamError } from '../../errors';
-import { badRequest, ok, serverError, unauthorized } from '../../helpers/http-helper';
+import { badRequest, ok, serverError, unauthorized } from '../../helpers/http/http-helper';
 import { LoginController } from './login';
 import { Authentication, HttpRequest, Validation } from './login-protocols';
 
