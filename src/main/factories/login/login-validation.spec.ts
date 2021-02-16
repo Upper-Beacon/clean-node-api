eb58cb6502d78b6d6fb55c3a69cf89745a662d3e
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-16 19:18:09 -0300

    refactor: move Validation to protocols folder

--- src/main/factories/login/login-validation.spec.ts
+++ src/main/factories/login/login-validation.spec.ts
@@ -1,11 +1,11 @@
-import { EmailValidation } from '../../../presentation/helpers/validators/email-validation';
-import { RequiredFieldValidation } from '../../../presentation/helpers/validators/required-field-validation';
-import { Validation } from '../../../presentation/helpers/validators/validation';
-import { ValidationComposite } from '../../../presentation/helpers/validators/validation-composite';
 import { EmailValidator } from '../../../presentation/protocols/email-validator';
+import { EmailValidation } from '../../../presentation/protocols/validators/email-validation';
+import { RequiredFieldValidation } from '../../../presentation/protocols/validators/required-field-validation';
+import { Validation } from '../../../presentation/protocols/validators/validation';
+import { ValidationComposite } from '../../../presentation/protocols/validators/validation-composite';
 import { makeLoginValidation } from './login-validation';
 
-jest.mock('../../../presentation/helpers/validators/validation-composite');
+jest.mock('../../../presentation/protocols/validators/validation-composite');
 
 const makeEmailValidator = (): EmailValidator => {
   class EmailValidatorStub implements EmailValidator {
