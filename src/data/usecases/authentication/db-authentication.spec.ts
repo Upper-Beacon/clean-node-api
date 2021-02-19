3a2e68991e76db8a054a4f6e44dad81726c7df18
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-19 19:02:39 -0300

    test: ensure DbAuthentication returns an access token on success

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -130,4 +130,10 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication());
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return an accessToken on success', async () => {
+    const { sut } = makeSut();
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBe('any_token');
+  });
 });
