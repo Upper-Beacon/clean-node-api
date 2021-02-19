6a74058a7728569bd613c666f5ca470ea84951ff
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-19 18:09:32 -0300

    test: ensure DbAuthentication calls LoadAccountByEmailRepository with correct email

---
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -0,0 +1,28 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AccountModel } from '../../../domain/models/account';
+import { LoadAccountByEmailRepository } from '../../protocols/load-account-by-email-repository';
+import { DbAuthentication } from './db-authentication';
+
+describe('DbAuthentication UseCase', () => {
+  test('Should call LoadAccountByEmailRepository with correct email', async () => {
+    class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+      async load(email: string): Promise<AccountModel> {
+        const account: AccountModel = {
+          id: 'any_id',
+          name: 'any_name',
+          email: 'any_email@mail.com',
+          password: 'any_password',
+        };
+        return new Promise(resolve => resolve(account));
+      }
+    }
+    const loadAccountbyEmailRepositoryStub = new LoadAccountByEmailRepositoryStub();
+    const sut = new DbAuthentication(loadAccountbyEmailRepositoryStub);
+    const loadSpy = jest.spyOn(loadAccountbyEmailRepositoryStub, 'load');
+    await sut.auth({
+      email: 'any_email@mail.com',
+      password: 'any_password',
+    });
+    expect(loadSpy).toHaveBeenCalledWith('any_email@mail.com');
+  });
+});
