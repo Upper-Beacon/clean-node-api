8683afc615842ee88b3fc4b2933e343bd475ff27
Danilo Augusto <dasf_cm@terra.com.br>
2021-02-19 18:30:42 -0300

    test: ensure DbAuthentication returns null if LoadAccountByEmailRepository returns null

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -57,4 +57,11 @@ describe('DbAuthentication UseCase', () => {
     const promise = sut.auth(makeFakeAuthentication());
     await expect(promise).rejects.toThrow();
   });
+
+  test('Should return null if LoadAccountByEmailRepository returns null', async () => {
+    const { sut, loadAccountbyEmailRepositoryStub } = makeSut();
+    jest.spyOn(loadAccountbyEmailRepositoryStub, 'load').mockReturnValueOnce(null);
+    const accessToken = await sut.auth(makeFakeAuthentication());
+    expect(accessToken).toBeNull();
+  });
 });
