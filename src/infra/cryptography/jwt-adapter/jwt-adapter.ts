e24eca4ffafa54f18eb7d680c41a0d7f5c626616
Danilo Augusto <dasf_cm@terra.com.br>
2021-03-01 11:42:28 -0300

    refactor: remove comment

--- src/infra/cryptography/jwt-adapter/jwt-adapter.ts
+++ src/infra/cryptography/jwt-adapter/jwt-adapter.ts
@@ -7,7 +7,7 @@ export class JwtAdapter implements Encrypter {
   constructor(secret: string) {
     this.secret = secret;
   }
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+
   async encrypt(value: string): Promise<string> {
     const accessToken = await jwt.sign({ id: value }, this.secret);
     return accessToken;
