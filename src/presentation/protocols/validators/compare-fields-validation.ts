95bfe3bc209b0b7adc7fb8030e1000cb4625ae62
Danilo Augusto <dasf_cm@terra.com.br>
2021-03-02 10:19:36 -0300

    refactor: remove duplicated code

--- src/presentation/protocols/validators/compare-fields-validation.ts
+++ src/presentation/protocols/validators/compare-fields-validation.ts
@@ -2,13 +2,8 @@ import { InvalidParamError } from '../../errors';
 import { Validation } from './validation';
 
 export class CompareFieldsValidation implements Validation {
-  private readonly fieldName: string;
-  private readonly fieldToCompareName: string;
+  constructor(private readonly fieldName: string, private readonly fieldToCompareName: string) {}
 
-  constructor(fieldName: string, fieldToCompareName: string) {
-    this.fieldName = fieldName;
-    this.fieldToCompareName = fieldToCompareName;
-  }
   validate(input: any): Error {
     return input[this.fieldName] !== input[this.fieldToCompareName]
       ? new InvalidParamError(this.fieldToCompareName)
--- src/presentation/protocols/validators/email-validation.ts
+++ src/presentation/protocols/validators/email-validation.ts
@@ -3,13 +3,7 @@ import { EmailValidator } from '../email-validator';
 import { Validation } from './validation';
 
 export class EmailValidation implements Validation {
-  private readonly fieldName: string;
-  private readonly emailValidator: EmailValidator;
-
-  constructor(fieldName: string, emailValidator: EmailValidator) {
-    this.fieldName = fieldName;
-    this.emailValidator = emailValidator;
-  }
+  constructor(private readonly fieldName: string, private readonly emailValidator: EmailValidator) {}
 
   validate(input: any): Error {
     return !this.emailValidator.isValid(input[this.fieldName]) ? new InvalidParamError(this.fieldName) : null;
